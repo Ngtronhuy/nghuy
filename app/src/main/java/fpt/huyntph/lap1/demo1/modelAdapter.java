1631f7ce32aed7e53fbe2f15845196952f901de5
Ngtronhuy <huyh87611@gmail.com>
2024-06-26 16:38:29 +0700

    commit 1

---
+++ app/src/main/java/fpt/huyntph/lap1/demo1/modelAdapter.java
@@ -0,0 +1,33 @@
+package fpt.huyntph.lap1.demo1;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import fpt.huyntph.lap1.R;
+import androidx.annotation.NonNull;
+
+
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class modelAdapter extends ArrayAdapter<model> {
+    public modelAdapter(@NonNull Context context, @NonNull List<model> models) {
+        super(context,0, models);
+    }
+
+    public View getView(int position, @Nullable View convertview, @NonNull ViewGroup parent){
+        if (convertview == null){
+            convertview = LayoutInflater.from(getContext()).inflate(R.layout.demo11itemview,parent,false);
+        }
+        model model = getItem(position);
+        TextView tvTitel = convertview.findViewById(R.id.demo11_item_tvTitel);
+        TextView tvDes = convertview.findViewById(R.id.demo11_item_tvDes);
+        tvTitel.setText(model.getTitel());
+        tvDes.setText(model.getDes());
+        return convertview;
+    }
+}
